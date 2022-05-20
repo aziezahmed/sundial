10523d8bc62c3de33a327e32c340df69a464a725
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2022-05-20 17:32:45 +0100

    add times

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
 import express from 'express';
 import SunCalc from 'suncalc';
+import { DateTime } from "luxon";
+import { get } from 'lodash';
 
 const app = express();
 const port = 3000;
 
-
 app.get('/', (req, res) => {
 
-  SunCalc.addTime(-18, 'dawnAstronomical', 'duskAstronomical');
-  SunCalc.addTime(-12, 'dawnNautical', 'duskNautical');
-  SunCalc.addTime(-6, 'dawnCivil', 'duskCivil');
+  SunCalc.addTime(-18, 'astronomicalDawn', 'astrologicalDusk');
+  SunCalc.addTime(-6, 'civilDawn', 'civilDusk');
 
-  const times = SunCalc.getTimes(new Date(), 51.5, -0.116);
+  const times = SunCalc.getTimes(new Date(), 51.51, -0.057);
 
-  res.send(times);
+  const rval = {
+    astronomicalDawn: DateTime.fromJSDate(get(times, 'astronomicalDawn')).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    nauticalDawn: DateTime.fromJSDate(times.nauticalDawn, 'civiDawn').plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    civilDawn: DateTime.fromJSDate(get(times, 'civilDawn')).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    sunrise: DateTime.fromJSDate(times.sunrise).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    noon: DateTime.fromJSDate(times.solarNoon).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    sunset: DateTime.fromJSDate(times.sunset).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    civilDusk: DateTime.fromJSDate(get(times, 'civilDusk')).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    nauticalDusk: DateTime.fromJSDate(times.nauticalDusk).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    astrologicalDusk: DateTime.fromJSDate(get(times, 'astrologicalDusk')).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+    nadir: DateTime.fromJSDate(times.nadir).plus({ hours: 1 }).toLocaleString(DateTime.TIME_24_SIMPLE),
+  }
+
+  res.send(rval);
 });
 
 app.listen(port, () => {
